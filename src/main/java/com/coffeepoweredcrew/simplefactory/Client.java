0ba3057170aa6e63df9dffddc20c854ccac39ea7
Phil Dolanov <phildolganov@yahoo.com>
2020-08-22 14:25:02 -0700

    added simple factory example

---
+++ src/main/java/com/coffeepoweredcrew/simplefactory/Client.java
@@ -0,0 +1,9 @@
+package com.coffeepoweredcrew.simplefactory;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Post post = PostFactory.createPost("blog");
+        System.out.println(post);
+    }
+}
