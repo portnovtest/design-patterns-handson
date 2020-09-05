e577158f884010006f089bdb323fd64f1f175979
Phil Dolanov <phildolganov@yahoo.com>
2020-09-05 13:10:40 -0700

    added Decorator design pattern

---
+++ src/main/java/com/coffeepoweredcrew/decorator/HtmlEncodedMessage.java
@@ -0,0 +1,19 @@
+package com.coffeepoweredcrew.decorator;
+
+import org.apache.commons.text.StringEscapeUtils;
+
+//Decorator. Implements component interface
+public class HtmlEncodedMessage implements Message {
+
+    private final Message msg;
+
+    public HtmlEncodedMessage(Message msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String getContent() {
+
+        return StringEscapeUtils.escapeHtml4(msg.getContent());
+    }
+}
