af98b57f3fd3d864b27ff64c83ac9507c245e14d
Phil Dolanov <phildolganov@yahoo.com>
2020-09-13 15:51:58 -0700

    added Observer design pattern

---
+++ src/main/java/com/coffeepoweredcrew/observer/Client.java
@@ -0,0 +1,13 @@
+package com.coffeepoweredcrew.observer;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Order order = new Order("100");
+        PriceObserver price = new PriceObserver();
+        order.attach(price);
+        order.addItem(50);
+        order.addItem(179);
+        System.out.println(order);
+    }
+}
---
+++ src/main/java/com/coffeepoweredcrew/observer/Order.java
@@ -0,0 +1,66 @@
+package com.coffeepoweredcrew.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//A concrete subject 
+public class Order {
+
+    private final String id;
+
+    private double shippingCost;
+    //cost of items
+    private double itemCost;
+
+    private double discount;
+    //no of items
+    private int count;
+
+    private final List<OrderObserver> observers = new ArrayList<>();
+
+    public void attach(OrderObserver observer) {
+        observers.add(observer);
+    }
+
+    public void detach(OrderObserver observer) {
+        observers.remove(observer);
+    }
+
+    public Order(String id) {
+        this.id = id;
+    }
+
+    public double getTotal() {
+        return itemCost - discount + shippingCost;
+    }
+
+    public void addItem(double price) {
+        itemCost += price;
+        count++;
+        observers.forEach(o -> o.updated(this));
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setShippingCost(double cost) {
+        this.shippingCost = cost;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getItemCost() {
+        return itemCost;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Order#" + id + "\nItem cost:" + itemCost + "\nNo. of items:" + count
+                + "\nShipping cost:" + shippingCost + "\nDiscount:" + discount
+                + "\nTotal:" + getTotal();
+    }
+}
