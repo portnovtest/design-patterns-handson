fd628881d694f8aa2d680d2de20e447a3894801e
Phil Dolanov <phildolganov@yahoo.com>
2020-09-27 12:30:45 -0700

    added null object pattern

---
+++ src/main/java/com/coffeepoweredcrew/nullobject/NullStorageService.java
@@ -0,0 +1,9 @@
+package com.coffeepoweredcrew.nullobject;
+
+public class NullStorageService extends StorageService {
+
+    @Override
+    public void save(Report report) {
+        System.out.println("Null object save method. Doing nothing");
+    }
+}
---
+++ src/main/java/com/coffeepoweredcrew/nullobject/Report.java
@@ -0,0 +1,14 @@
+package com.coffeepoweredcrew.nullobject;
+
+public class Report {
+
+    private final String name;
+
+    public Report(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
